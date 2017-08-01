59884841d4bf3e42197dca649d4aba72ee548cfc
Gerardo Nardelli <patitonardelli@gmail.com>
2017-07-31 23:36:01 -0300

    feat: Add category change functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MyReads from './components/MyReads'
 import Search from './components/Search'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
+  state = {
+    categories: [
+      {
+        id: 'currentlyReading',
+        name: 'Currently Reading',
+        list: []
+      },
+      {
+        id: 'wantToRead',
+        name: 'Want to Read',
+        list: []
+      },
+      {
+        id: 'read',
+        name: 'Read',
+        list: []
+      }
+    ]
+  }
+
+  handleCategoryChange = (book, currentCategory, newCategory) => {
+    const categories = this.state.categories
+    const newCategories = categories.map(category => {
+      if(category.id === currentCategory) {
+        category.list = category.list.filter((b)=> b.id !== book.id)
+      } else if(category.id === newCategory) {
+        category.list = category.list.concat([book])
+      }
+      return category
+    })
+
+    this.setState({categories: newCategories})
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <MyReads />
+          <MyReads handleCategoryChange={this.handleCategoryChange} {...this.state}/>
         )}/>
         <Route exact path='/search' render={() => (
           <Search/>
